30c9e7f4c9fc9901e49474dbc06fa0cec6ac13c1
Tori <torihendricks927@yahoo.com>
2022-04-12 10:03:42 -0400

    getting button registered in js

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 var searchFormEl = document.querySelector("#search-form");
 var weatherDayCityEl = document.querySelector("#weather-day-city");
-var weatherDayTempEl = document.querySelector("weather-day-temp");
+var weatherDayTempEl = document.querySelector("#weather-day-temp");
 var weatherDayWindEl = document.querySelector("#weather-day-wind");
 var weatherDayUvIndexEl = document.querySelector("#weather-day-uv-index");
 var weatherDayHumidityEl = document.querySelector("#weather-day-humidity");
@@ -13,16 +13,46 @@ var WeatherDayIconEl = document.querySelector("#weather-day-icon");
 var weatherDayContainerEl = document.querySelector("#weather-day-container");
 var outerForecastContainerEL = document.querySelector("#outer-forecast-container");
 
+// creates the 5 day box forcast below top container
 function populateFiveDay(data) {
-    data.forEach(function(day) {
-        var current = data.current;
-        var temp = current.temp;
-        var windSpeed = current.wind_speed;
-        var humidity = current.humidity;
-        var uviIndex = current.uvi;
-    })
+    forecastContainerEl.innerHTML = "";
+    data.forEach(function(day, index) {
+        if (index === 0 || index > 5) {
+            return;
+        }
+        var dt = day.dt;
+        var temp = day.temp.day;
+        var windSpeed = day.wind_speed;
+        var humidity = day.humidity;
+        var uviIndex = day.uvi;
+        var date = moment(dt * 1000).format("L");
+        var icon = day.weather[0].icon;
+        var div = document.createElement("div");
+        var offsetClass = "";
+        if (index === 1) {
+          offsetClass = "col-lg-offset-1";
+        }
+            div.classList = `card-weather-container col-sm-12 ${offsetClass} col-lg-2 text-light`;
+            div.innerHTML = `
+                        <div class="card-weather bg-dark p-3">
+                            <h4>${date}</h4>
+                            <img src="https://openweathermap.org/img/wn/${icon}.png" />
+                            <dl>
+                                <dt>Temp:</dt>
+                                <dd>${temp}</dd>
+                                <dt>Wind:</dt>
+                                <dd>${windSpeed} MPH</dd>
+                                <dt>Humidity</dt>
+                                <dd>${humidity}%</dd>
+                            </dl>
+                        </div>
+                        `;
+                        forecastContainerEl.appendChild(div);
+    });
+    outerForecastContainerEL.classList.remove("hide");
 }
 
+// creates process of fetching API to obtain information searched
 function getWeatherApi(city) {
     var requestUrl= `${baseUrl}geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
          console.log(requestUrl)
@@ -69,6 +99,8 @@ function getWeatherApi(city) {
     });
 }
 
+
+// saves past search history to local storage
 function saveCitySearch(city) {
     city = city.toLowerCase();
     var cities = window.localStorage.getItem("cities");
@@ -88,8 +120,33 @@ function saveCitySearch(city) {
     window.localStorage.setItem("cities", JSON.stringify(cities));
 }
 
+function populateButtons() {
+    buttonContainerEl.innerHTML = "";
+    var cities = window.localStorage.getItem("cities");
+    if (cities) {
+        cities = JSON.parse(cities);
+    }
+    else {
+        cities = [];
+    }
 
-var FormSubmitHandler = function(event) {
+    cities.forEach(function (city) {
+        var button = document.createElement("button");
+        button.classList = "btn btn-secondary col-12";
+        button.textContent = city;
+        button.setAttribute("data-city", city);
+        buttonContainerEl.appendChild(button);
+    });
+}
+
+function handleButtonClick(event) {
+    var target = event.target;
+    var city = target.getAttribute("data-city");
+    getWeatherApi(city);
+}
+
+
+function FormSubmitHandler(event) {
     event.preventDefault();
     var city = searchFormCityInputEl.value;
     if (!city) {
